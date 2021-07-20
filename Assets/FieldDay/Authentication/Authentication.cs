761c4785d0425826f19aabfad0fe7c1d5459adf2
nspevacek <nspevacek@wisc.edu>
2021-07-20 14:04:37 -0500

    Update accessors, add default id values for testing

--- Assets/FieldDay/Authentication/Authentication.cs
+++ Assets/FieldDay/Authentication/Authentication.cs
@@ -2,40 +2,40 @@
 
 public class Authentication : MonoBehaviour
 {
-    private string m_SaveCode;
-    private string m_PlayerName;
-    private string m_ClassCode;
-
-    // TODO: may need to access these from AnalyticsService to send with log events?
+    private string m_SaveCode = "SaveCode";
+    private string m_PlayerName = "PlayerName";
+    private string m_ClassCode = "ClassCode";
     
-    // set when new game is clicked / continue clicked and player enters their code
-    public string SaveCode { get; set; }
-    // set when player enters for new game
-    public string PlayerName { get; set; }
-    // set when loading from class URL
-    public string ClassCode { get; set; }
+    #region Accessors
 
-    private string GetSaveCode()
+    // set when new game is clicked / continue clicked and player enters their code
+    public string SaveCode { get { return m_SaveCode; } set { m_SaveCode = value; } }
+    // set when player enters for new game
+    public string PlayerName { get { return m_PlayerName; } set { m_PlayerName = value; } }
+    // set when loading from class URL
+    public string ClassCode { get { return m_ClassCode; } set { m_ClassCode = value; } }
+
+    #endregion // Accessors
+
+    private string GenerateSaveCode()
     {
         // call id gen API, set m_SaveCode, return to display in UI
-        return "";
+        return m_SaveCode;
     }
 
-    // TODO: what format is player data saved as (JSON string, custom)?
-
-    private void SendPlayerData(string playerData, string saveCode)
+    private void SendPlayerData(TextAsset playerData, string saveCode)
     {
         // send save data for anon player with save code
     }
 
-    private void SendPlayerData(string playerData, string saveCode, string playerName, string classCode)
+    private void SendStudentData(TextAsset playerData, string saveCode, string playerName, string classCode)
     {
         // send save data for a student with name and class code
     }
 
-    private string LoadPlayerData(string saveCode)
+    private TextAsset LoadPlayerData(string saveCode)
     {
         // get save data for a given code from DB
-        return "";
+        return null;
     }
 }
