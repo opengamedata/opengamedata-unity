5fa358253f496a75f127fdfa83001bcf448429f8
nspevacek <nspevacek@wisc.edu>
2021-08-02 13:26:09 -0500

    Implement API calls

--- Assets/FieldDay/Authentication/Authentication.cs
+++ Assets/FieldDay/Authentication/Authentication.cs
@@ -7,54 +7,51 @@ public static class Authentication
     // Called once player hits start to display save code and cache somewhere
     public static void GenerateSaveCode(Action<string> completeCallback, Action<string> errorCallback)
     {
-        // UnityWebRequest request = UnityWebRequest.Get("url");
-        // AsyncOperation requestHandle = request.SendWebRequest();
-        // requestHandle.completed += (async) => {
-        //     if (request.isNetworkError || request.isHttpError)
-        //     {
-        //         errorCallback(request.error);
-        //     }
-        //     else
-        //     {
-        //         completeCallback(request.downloadHandler.text);
-        //     }
-        // };
-        completeCallback("SaveCode");
+        UnityWebRequest request = UnityWebRequest.Get("https://fieldday-web.wcer.wisc.edu/wsgi-bin/opengamedata.wsgi/player/createID");
+        AsyncOperation requestHandle = request.SendWebRequest();
+        requestHandle.completed += (async) => {
+            if (request.isNetworkError || request.isHttpError)
+            {
+                errorCallback(request.error);
+            }
+            else
+            {
+                completeCallback(request.downloadHandler.text);
+            }
+        };
     }
 
     // Send player data to server, use callbacks to check if post request errored or completed properly
-    public static void SendPlayerData(string playerData, string saveCode, string playerName, string classCode, Action<string> completeCallback, Action<string> errorCallback)
+    public static void SendPlayerData(string playerData, string saveCode, Action<string> completeCallback, Action<string> errorCallback)
     {
-        // UnityWebRequest request = UnityWebRequest.Post("url", "data");
-        // AsyncOperation requestHandle = request.SendWebRequest();
-        // requestHandle.completed += (async) => {
-        //     if (request.isNetworkError || request.isHttpError)
-        //     {
-        //         errorCallback(request.error);
-        //     }
-        //     else
-        //     {
-        //         completeCallback("Save complete");
-        //     }
-        // };
-        completeCallback("Save complete");
+        UnityWebRequest request = UnityWebRequest.Post($"https://fieldday-web.wcer.wisc.edu/wsgi-bin/opengamedata.wsgi/player/{saveCode}/state", playerData);
+        AsyncOperation requestHandle = request.SendWebRequest();
+        requestHandle.completed += (async) => {
+            if (request.isNetworkError || request.isHttpError)
+            {
+                errorCallback(request.error);
+            }
+            else
+            {
+                completeCallback(request.downloadHandler.text);
+            }
+        };
     }
 
     // Get player data from server, return with complete callback
-    public static void LoadPlayerData(string saveCode, string classCode, Action<string> completeCallback, Action<string> errorCallback)
+    public static void LoadPlayerData(string saveCode, Action<string> completeCallback, Action<string> errorCallback)
     {
-        // UnityWebRequest request = UnityWebRequest.Get("url");
-        // AsyncOperation requestHandle = request.SendWebRequest();
-        // requestHandle.completed += (async) => {
-        //     if (request.isNetworkError || request.isHttpError)
-        //     {
-        //         errorCallback(request.error);
-        //     }
-        //     else
-        //     {
-        //         completeCallback(request.downloadHandler.text);
-        //     }
-        // };
-        completeCallback("");
+        UnityWebRequest request = UnityWebRequest.Get($"https://fieldday-web.wcer.wisc.edu/wsgi-bin/opengamedata.wsgi/player/{saveCode}/state");
+        AsyncOperation requestHandle = request.SendWebRequest();
+        requestHandle.completed += (async) => {
+            if (request.isNetworkError || request.isHttpError)
+            {
+                errorCallback(request.error);
+            }
+            else
+            {
+                completeCallback(request.downloadHandler.text);
+            }
+        };
     }
 }
